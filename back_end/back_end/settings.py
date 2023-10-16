5012de8cc27a16e96c7a6fccc6f292f9951db4a6
zsert1 <zsert1@naver.com>
2023-10-16 09:15:08 -0400

    비밀번호 유저이름 회원가입

--- back_end/back_end/settings.py
+++ back_end/back_end/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'yx()euv+o3+_oux&_#2ha6yft(r%pny*+xz75m)%l&&69)&na#'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
+CORS_ORIGIN_ALLOW_ALL = True 
 
 # Application definition
 
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'account',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- froent_end/src/pages/account/signUp.tsx
+++ froent_end/src/pages/account/signUp.tsx
@@ -1,8 +1,57 @@
-import React from "react";
+import Axios from 'axios';
+import React, {useEffect, useState} from 'react';
+const SingUp = () => {
+  const [input, setInput] = useState({username: '', password: ''});
+  const [err, setErr] = useState({username: '', password: ''});
+  const [submitDisable, setSubmitDisable] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-const SingUp=()=>{
-    return <div>
-        회원가입
+  const onSubmit = e => {
+    e.preventDefault();
+    setErr(null);
+    setLoading(true);
+    Axios.post('http://127.0.0.1:8000/account/singup/', input)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err => {
+        if (err.response) {
+          setErr({
+            username: (err.response.data.username || []).join(''),
+            password: (err.response.data.password || []).join(''),
+          });
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+  useEffect(() => {
+    const isDisable = input.username.length === 0 || input.password.length === 0;
+    setSubmitDisable(isDisable);
+  }, [input]);
+  const onChange = e => {
+    const {name, value} = e.target;
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div>
+          <input type="text" name="username" onChange={onChange} />
+          {err?.username}
+        </div>
+        <div>
+          <input type="password" name="password" onChange={onChange} />
+          {err?.password}
+          {}
+        </div>
+        <input type="submit" value="회원가입" disabled={loading || submitDisable} />
+      </form>
     </div>
-}
-export default SingUp
+  );
+};
+export default SingUp;
