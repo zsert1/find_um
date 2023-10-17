9d0e3867775c85232ee91b5db9f2043d860cdd0a
zsert1 <zsert1@naver.com>
2023-10-17 08:50:51 -0400

    ant 디자인 활용

--- froent_end/src/pages/account/signUp.tsx
+++ froent_end/src/pages/account/signUp.tsx
@@ -1,57 +1,77 @@
 import Axios from 'axios';
 import React, {useEffect, useState} from 'react';
+import {Button, Form, Input, notification} from 'antd';
+type FieldType = {
+  username?: string;
+  password?: string;
+};
 const SingUp = () => {
-  const [input, setInput] = useState({username: '', password: ''});
-  const [err, setErr] = useState({username: '', password: ''});
-  const [submitDisable, setSubmitDisable] = useState(true);
-  const [loading, setLoading] = useState(true);
-
-  const onSubmit = e => {
-    e.preventDefault();
-    setErr(null);
-    setLoading(true);
-    Axios.post('http://127.0.0.1:8000/account/singup/', input)
-      .then(response => {
-        console.log(response);
-      })
-      .catch(err => {
-        if (err.response) {
-          setErr({
-            username: (err.response.data.username || []).join(''),
-            password: (err.response.data.password || []).join(''),
+  // const [filedErrors, setFieldErrors] = useState({});
+  const onFinish = values => {
+    const fn = async () => {
+      const {username, password} = values;
+      const data = {username, password};
+      try {
+        await Axios.post('http://127.0.0.1:8000/account/singup/', data);
+        notification.open({
+          message: '회원가입 성공',
+          description: '로그인 페이지로 이동합니다',
+        });
+      } catch (error) {
+        if (error.response) {
+          notification.open({
+            message: '회원가입 실패',
+            description: '아이디/비밀번호를 확인해주세요',
           });
+          // const {data: fieldsErrorMessage} = error.response;
+          // setFieldErrors(
+          //   Object.entries(fieldsErrorMessage).reduce((acc, [fieldName, errors]) => {
+          //     acc[fieldName] = {
+          //       validateStatus: 'errors',
+          //       [fieldName]: errors,
+          //     };
+          //     return acc;
+          //   }, {}),
+          // );
         }
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+      }
+    };
+    fn();
   };
-  useEffect(() => {
-    const isDisable = input.username.length === 0 || input.password.length === 0;
-    setSubmitDisable(isDisable);
-  }, [input]);
-  const onChange = e => {
-    const {name, value} = e.target;
-    setInput({
-      ...input,
-      [name]: value,
-    });
-  };
+
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <div>
-          <input type="text" name="username" onChange={onChange} />
-          {err?.username}
-        </div>
-        <div>
-          <input type="password" name="password" onChange={onChange} />
-          {err?.password}
-          {}
-        </div>
-        <input type="submit" value="회원가입" disabled={loading || submitDisable} />
-      </form>
-    </div>
+    <Form
+      labelCol={{span: 8}}
+      wrapperCol={{span: 16}}
+      style={{maxWidth: 600}}
+      // initialValues={{remember: true}}
+      onFinish={onFinish}
+      // onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item<FieldType>
+        label="Username"
+        name="username"
+        rules={[{required: true, message: '5글자 이상'}]}
+        hasFeedback
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item<FieldType>
+        label="Password"
+        name="password"
+        rules={[{required: true, message: 'Please input your password!'}]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item wrapperCol={{offset: 8, span: 16}}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
   );
 };
 export default SingUp;
