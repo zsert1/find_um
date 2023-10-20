57e790b66ca3dbed9f642ab55bffffe11bdc9c93
zsert1 <zsert1@naver.com>
2023-10-20 10:46:25 -0400

    jwt설정

--- back_end/account/urls.py
+++ back_end/account/urls.py
@@ -1,6 +1,13 @@
 
-from django.urls import path
+from django.urls import include, path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 urlpatterns = [
-    path('singup/',views.SingupView.as_view(),name="login")
+    path('singup/',views.SingupView.as_view(),name="login"),
+    path('registration/', include('dj_rest_auth.registration.urls')),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+   
 ]
