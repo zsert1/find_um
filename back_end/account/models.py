c2e0dba2343d56601b425b863d8647e91b6485a5
zsert1 <zsert1@naver.com>
2023-12-01 07:27:15 -0500

    기초부터 다시 시작

--- back_end/account/models.py
+++ back_end/account/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+class Post(models.Model):
+    message=models.TextField()
+    create_at=models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now=True)
 # Create your models here.
--- back_end/account/urls.py
+++ back_end/account/urls.py
@@ -1,13 +1,15 @@
 
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 from . import views
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+router=DefaultRouter()
+router.register('post',views.PostViewSet) #2개의 URL을 만들어준다
 urlpatterns = [
-    path('singup/',views.SingupView.as_view(),name="login"),
-    path('registration/', include('dj_rest_auth.registration.urls')),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('',include(router.urls))
+    # path('singup/',views.SingupView.as_view(),name="login"),
+  
+
+    # path('registration/', include('dj_rest_auth.registration.urls')),
+    # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
    
 ]
