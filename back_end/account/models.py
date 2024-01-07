2acddb9e9ccd9f5fd320b78d7f9fac843437ed6e
zsert1 <zsert1@naver.com>
2024-01-07 22:47:31 +0900

    poetry로 변경

--- back_end/account/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Post(models.Model):
-    message=models.TextField()
-    create_at=models.DateTimeField(auto_now_add=True)
-    update_at=models.DateTimeField(auto_now=True)
-
